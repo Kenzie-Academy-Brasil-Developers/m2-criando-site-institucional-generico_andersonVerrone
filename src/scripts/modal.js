fa6ae9eff5f7409905943783da5c2963de22cc7a
andersonVerrone <doverrone@gmail.com>
2023-03-30 11:34:17 -0300

    atualizando erros

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,50 +1,24 @@
 function init() {
-	const btnModal = document.querySelectorAll('.btn__modal');
-	const desabledItems = document.querySelectorAll('body > :not(.modal)');
-	const modal = document.querySelector('.modal');
-	const closeModal = document.querySelector('.close');
-	btnModal.forEach(function(item) {
-		item.addEventListener('click', () => {
-			activeModal(desabledItems, modal);
-			centerModal(modal);
-		})
-	})
-	closeModal.addEventListener('click', () => {
-		disableModal(desabledItems, modal);
-	})
+    const btnActiveModal = document.querySelectorAll('.btn__activeModal');
+    const modal = document.querySelector('.modal');
+    const btnClose = document.querySelector('.close');
+
+    setShowModal(btnActiveModal,modal);
+    setCloseModal(btnClose , modal);
 }
 
-function disableModal(activeItems, modal) {
-	activedItems(activeItems);
-	modal.classList.add('hidden')
+function setShowModal(buttons , modal) {
+    buttons.forEach((element) => {
+        element.addEventListener('click', () => {
+            modal.showModal();
+        })
+    });
 }
 
-function activedItems(items) {
-	items.forEach(function(item) {
-		item.classList.remove('disabled')
-	})
+function setCloseModal(button , modal) {
+    button.addEventListener('click', () => {
+        modal.close();
+    })
 }
 
-function activeModal(itemsToBeDeactivated, modal) {
-	deactivate(itemsToBeDeactivated);
-	modal.classList.remove('hidden')
-}
-
-function deactivate(items) {
-	items.forEach(function(item) {
-		item.classList.add('disabled')
-	})
-}
-
-function centerModal(element) {
-	const screenWidth = window.innerWidth;
-	const screenHeight = window.innerHeight;
-	const screenHeigthPosition = window.pageYOffset;
-	const centerX = screenWidth / 2;
-	const centerY = screenHeight / 2;
-	const modalX = centerX;
-	const modalY = screenHeigthPosition + centerY;
-	element.style.top = modalY + 'px';
-	element.style.left = modalX + 'px';
-}
 init();
